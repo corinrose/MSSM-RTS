e7e863beed7c02f445ac8a59f52af1314289cf26
Emmanuel Tung <tunge@mssm.org>
2014-12-07 16:23:21 -0500

    Top Down elements are functional
    the sprites only stay on the first sprite of the sprite sheet for some
    reason

--- rtslib/tdent.py
+++ rtslib/tdent.py
@@ -1,3 +1,6 @@
+import pygame
+from rtslib.sheet import *
+
 class tdent():
 	def __init__(self, posX, posY, desX, desY, speed, sheet):
 		self.pos = [posX, posY]
@@ -6,18 +9,16 @@ class tdent():
 		self.sheet = sheet
 		
 	def draw(self, surface):
-		surface.blit(self.sheet.getImage())
+		surface.blit(self.sheet.getImage(), (self.pos[0], self.pos[1]))
 	
 	def update(self):
-		"""if self.pos[0] != self.des[0]:
+		if self.pos[0] != self.des[0]:
 			if self.pos[0] < self.des[0] < self.pos[0] + self.speed or self.pos[0] + self.speed < self.des[0] < self.pos[0]:
 				self.pos[0] = self.des[0]
-	        else:
+			else:
 				self.pos[0] += self.speed
-		else:
-			if self.pos[1] != self.des[1]:
-				if self.pos[1] < self.des[1] < self.pos[1] + self.speed or self.pos[1] + self.speed < self.des[1] < self.pos[1]:
-					self.pos[1] = self.des[1] 
-				else:
-					self.pos[1] += self.speed """
-		pass
+		elif self.pos[1] != self.des[1]:
+			if self.pos[1] < self.des[1] < self.pos[1] + self.speed or self.pos[1] + self.speed < self.des[1] < self.pos[1]:
+				self.pos[1] = self.des[1]
+			else:
+				self.pos[1] += self.speed
--- rtslib/tdworld.py
+++ rtslib/tdworld.py
@@ -1,17 +1,22 @@
 import pygame
+from rtslib.tdent import *
+from rtslib.sheet import *
 
 class tdworld():
 	def __init__(self):
 		self.entities = []
+		self.f = False # for spritesheet alternating flipping
 		
 	def update(self, events):
 	    for event in events:
-		    if event == pygame.mouseButtonDown:
-			    tdent(200, 200, 400, 400, 1, sheet("resources/stickman.png", [32, 32])) ##### testing
-	    for i in self.entities:
-		    self.entities[i].update()
+			if event.type == pygame.MOUSEBUTTONDOWN:
+				self.entities.append(tdent(200, 200, 400, 400, 0.25, sheet("resources/stickman.png", [32, 32]))) ### testing purposes
+				self.entities[-1].sheet.setFlipped(self.f)
+				self.f = not self.f	
+	    for ent in self.entities:
+		    ent.update()
 	
 	def draw(self, surface):
-		for i in self.entities:
-			self.entities[i].draw(surface)
+		for ent in self.entities:
+			ent.draw(surface)
 		
