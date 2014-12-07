1f1301b7ee7f997ca6b1354a1d7eec30956acaf1
Emmanuel Tung <tunge@mssm.org>
2014-12-07 13:40:45 -0500

    Started on TD elements
    Added the very basic tdworld framework, tdent attributes, draw function,
    update function

--- rtslib/tdent.py
+++ rtslib/tdent.py
@@ -1,4 +1,23 @@
 class tdent():
-	def __init__(self, x, y, sheet):
-		self.pos = [x,y]
+	def __init__(self, posX, posY, desX, desY, speed, sheet):
+		self.pos = [posX, posX]
+		self.des = [desX, desY]
+		self.speed = speed
 		self.sheet = sheet
+		
+    def draw(surface):
+	    surface.blit(self.sheet.getImage())
+	
+	def update(events):
+	    if (self.posX != self.desX)
+	        if (self.posX < self.desX < self.posX + self.speed or self.posX + self.speed < self.desX < self.posX):
+		        self.posX = self.desX
+	        else:
+			    self.posX += self.speed
+			    
+		elif (self.posY != self.desX)
+		    if (self.posY < self.desY < self.posY + self.speed or self.posY + self.speed < self.desY < self.posY):
+		        self.posY = self.desY 
+	        else:
+			    self.posY += self.speed 
+		
--- rtslib/tdworld.py
+++ rtslib/tdworld.py
@@ -2,9 +2,14 @@ class tdworld():
 	def __init__(self):
 		self.entities = []
 		
-	def update(self):
-		pass
+	def update(self, events):
+	    for event in events:
+		    if event == pygame.mouseButtonDown:
+			    tdent(200, 200, 400, 400, 1, sheet("resources/stickman.png", [32, 32])) ##### testing
+	    for i in self.entities:
+		    self.entities[i].update(events)
 	
 	def draw(self, surface):
-		pass
+		for i in self.entities:
+			self.entities[i].draw(surface)
 		
