2e5f335436ec073b2ab5fadee0f3e5b497b004c2
Emmanuel Tung <tunge@mssm.org>
2014-12-08 20:58:03 -0500

    topdown - added basic unit selecting/commanding
    topdown mode has selection markers, unit destination setting

--- rtslib/tdent.py
+++ rtslib/tdent.py
@@ -2,23 +2,34 @@ import pygame
 from rtslib.sheet import *
 
 class tdent():
-	def __init__(self, posX, posY, desX, desY, speed, sheet):
+	def __init__(self, posX, posY, desX, desY, isSelected, selectionMarker, speed, sheet):
 		self.pos = [posX, posY]
 		self.des = [desX, desY]
+		self.isSelected = isSelected 
+		self.selectionMarker = selectionMarker 
 		self.speed = speed
 		self.sheet = sheet
 		
 	def draw(self, surface):
-		surface.blit(self.sheet.getImage(), (self.pos[0], self.pos[1]))
+		surface.blit(self.sheet.getImage(), self.pos)
+		if self.isSelected:
+			surface.blit(self.selectionMarker, self.pos)
 	
 	def update(self):
 		if self.pos[0] != self.des[0]:
 			if self.pos[0] < self.des[0] < self.pos[0] + self.speed or self.pos[0] + self.speed < self.des[0] < self.pos[0]:
 				self.pos[0] = self.des[0]
-			else:
+			elif self.pos[0] < self.des[0]:
 				self.pos[0] += self.speed
+			else:
+				self.pos[0] -= self.speed
 		elif self.pos[1] != self.des[1]:
 			if self.pos[1] < self.des[1] < self.pos[1] + self.speed or self.pos[1] + self.speed < self.des[1] < self.pos[1]:
 				self.pos[1] = self.des[1]
-			else:
+			elif self.pos[1] < self.des[1]:
 				self.pos[1] += self.speed
+			else:
+				self.pos[1] -= self.speed
+	
+	def setDes(self, des):
+		self.des[0], self.des[1] = des
--- rtslib/tdworld.py
+++ rtslib/tdworld.py
@@ -10,13 +10,22 @@ class tdworld():
 	def update(self, events):
 	    for event in events:
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				self.entities.append(tdent(200, 200, 400, 400, 0.25, sheet("resources/stickman.png", [32, 32]))) ### testing purposes
-				self.entities[-1].sheet.setFlipped(self.f)
-				self.f = not self.f	
+				if event.button == 1: # left-click
+					self.spawnWorker();
+				if event.button == 3: # right-click
+					for ent in self.entities:
+						if ent.isSelected:
+							ent.setDes(pygame.mouse.get_pos())
 	    for ent in self.entities:
 		    ent.update()
 	
 	def draw(self, surface):
 		for ent in self.entities:
 			ent.draw(surface)
+			
+	def spawnWorker(self):
+		# resources -= 10
+		self.entities.append(tdent(200, 200, 400, 400, True, pygame.image.load("resources/selectionMarker.png").convert_alpha(), 0.25, sheet("resources/stickman.png", [32, 32]))) # worker
+		self.entities[-1].sheet.setFlipped(self.f)
+		self.f = not self.f
 		
