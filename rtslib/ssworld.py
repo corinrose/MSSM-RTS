694237b24dbec30c8badcef29e61a963ea9227bd
Andrew Reilley <areill1337@gmail.com>
2014-12-11 19:00:31 -0500

    Stopped flipping spawned SS entities, increased move speed

--- rtslib/ssworld.py
+++ rtslib/ssworld.py
@@ -9,14 +9,10 @@ class ssworld():
 		self.testpath = path([[30,30],[100,50],[200,150],[250,100],[375, 125]])
 		self.ssentities = []
 		
-		self.f = False
-		
 	def update(self, events):
 		for event in events:
 			if event.type == pygame.MOUSEBUTTONDOWN:
 				self.ssentities.append(ssent(0.0, sheet("resources/stickman.png", [32,32]), self.testpath))
-				self.ssentities[-1].sheet.setFlipped(self.f)
-				self.f = not self.f	
 				
 		for ent in self.ssentities:
 			ent.update()
--- rtslib/tdworld.py
+++ rtslib/tdworld.py
@@ -10,7 +10,7 @@ class tdworld():
 	def update(self, events):
 	    for event in events:
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				self.entities.append(tdent(200, 200, 400, 400, 0.25, sheet("resources/stickman.png", [32, 32]))) ### testing purposes
+				self.entities.append(tdent(200, 200, 400, 400, 0.5, sheet("resources/stickman.png", [32, 32]))) ### testing purposes
 				self.entities[-1].sheet.setFlipped(self.f)
 				self.f = not self.f	
 	    for ent in self.entities:
