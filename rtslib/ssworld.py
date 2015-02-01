d3b5fedc5c358c7510a2c237961f085ba58cc818
Andrew Reilley <areill1337@gmail.com>
2015-01-31 21:24:42 -0500

    Changed gold icon, put in skeleton enemy
    They are archers, so I will make them use ranged attacks soon.

--- rtslib/ssworld.py
+++ rtslib/ssworld.py
@@ -107,7 +107,7 @@ class ssworld():
 			if self.script[self.currentop]["command"] == "spawn" or self.script[self.currentop]["command"] == "spawnwave": #Spawning enemies, or waiting between spawns in a "wave"
 				if self.currentwavespawned < len(self.spawnqueue):
 					if self.scripttimer == 0:
-						self.ssentities.append(ssent(self.cid, self.path.length-1, -float(self.units[self.spawnqueue[self.currentwavespawned]]["properties"][1]), 20, sheet("resources/badstick.png", [32,32]), self.path, False, float(self.units[self.spawnqueue[self.currentwavespawned]]["properties"][0]), self.testattack))
+						self.ssentities.append(ssent(self.cid, self.path.length-1, -float(self.units[self.spawnqueue[self.currentwavespawned]]["properties"][1]), 20, sheet("resources/skeleman.png", [40,40]), self.path, False, float(self.units[self.spawnqueue[self.currentwavespawned]]["properties"][0]), self.testattack))
 						self.cid += 1
 						self.currentwavespawned += 1
 						if self.script[self.currentop]["command"] == "spawn":
