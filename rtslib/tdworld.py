c1fecfdf2743b021152f15d02a9200616956e61b
TungE <tunge@mssm.org>
2015-02-27 17:04:16 -0500

    slight cleanups in tdworld code

--- rtslib/tdworld.py
+++ rtslib/tdworld.py
@@ -20,26 +20,13 @@ def loadCFG(file):
 	for i in range(Ustart+1, Uend, 1):
 		details.append(lines[i].split(" "))
 	config["units"] = details
-	print details
-	#return {"units" : [[600, 300, 600, 300, True, 0, "resources/buildings/TownHall.png", [160,160], "Town Hall.", 1.1]]}
 	return config 
 	
 		
 class tdworld():
 	def __init__(self, game, path="resources/testlevel"):
-		'''
-		self.entities = [tdent(400, 400, 400, 400, 
-							   True, 0, sheet("resources/buildings/TownHall.png", [160,160]), 
-							   "Town Hall.", 1.1), 
-						tdent(50, 50, 200, 200, 
-							   False, 0, sheet("resources/Wood.png", [160,160]), 
-							   "Wood.", 2.2), 
-						tdent(500, 270, 270, 270, 
-							   False, 0, sheet("resources/Gold.png", [80,80]), 
-							   "Gold.", 2.3)] '''
 		self.cfg = loadCFG(path+"/tdworld.cfg")
 		self.entities = []
-		self.cfg["units"][0][6] = "resources/buildings/TownHall.png"
 		for i in range(len(self.cfg["units"])):
 			self.entities.append(tdent(int(self.cfg["units"][i][0]), int(self.cfg["units"][i][1]), int(self.cfg["units"][i][2]), int(self.cfg["units"][i][3]), 
 										bool(self.cfg["units"][i][4]), float(self.cfg["units"][i][5]), 
@@ -66,9 +53,11 @@ class tdworld():
 		self.selecting = False
 		### self.building = False ###
 		self.selectedAlready = False
+		'''
 		self.entities[0].buttons = [button("Spawn Worker", [125, 670], self.entities[0].addCommand, rtslib.common.buttonSets["hud"]),
 									button("Increase Pop", [235, 670], self.entities[0].addCommand, rtslib.common.buttonSets["hud"])] # one-time deal # WILL BE CONFIG-ED
-		self.levelPath = path 
+		'''
+		#self.levelPath = path 
 		
 	def update(self, events):
 		for event in events:
