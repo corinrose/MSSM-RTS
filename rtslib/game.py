3b596eb89206972f09ef947fee874faf712e4d00
Andrew Reilley <areill1337@gmail.com>
2015-02-28 02:47:17 -0500

    Added the pause menu
    Clicking the "menu" button no longer exits the current game immediately,
    but instead gives you the opportunity to either return to your game or
    go to the level select screen. This menu will also eventually have a
    settings button, but the settings menu will first need to have a way for
    its back button to go to the pause menu rather than always going to the
    main menu.

--- rtslib/game.py
+++ rtslib/game.py
@@ -10,7 +10,11 @@ class game():
 		self.buttons = [rtslib.button("switch", [1000,670], self.clickHandler, rtslib.common.buttonSets["hud"], "resources/fonts/Deutsch.ttf", "Swap"),
 						rtslib.button("menu", [1125,670], self.clickHandler, rtslib.common.buttonSets["hud"], "resources/fonts/Deutsch.ttf", "Menu")]
 		#Pause menu things
-		self.pauseButtons = []
+		self.paused = False
+		self.pauseback = pygame.surface.Surface([1280,720]).convert_alpha() #this is temporary
+		self.pauseback.fill([100, 100, 100, 100])
+		self.pauseButtons = [rtslib.button("menu", [480,200], self.clickHandler, rtslib.common.buttonSets["large"], "resources/fonts/Deutsch.ttf", "Main Menu"),
+							 rtslib.button("back", [480,350], self.clickHandler, rtslib.common.buttonSets["large"], "resources/fonts/Deutsch.ttf", "Back")]
 		#Endgame things
 		self.gameOver = False
 		self.gameOverBack = pygame.surface.Surface([1280, 720]).convert_alpha()
@@ -41,36 +45,45 @@ class game():
 			surface.blit(self.gameOverBack, [0,0])	
 			for button in self.gameOverButtons:
 				button.draw(surface)
+		#Pause menu
+		if self.paused:
+			surface.blit(self.pauseback, [0,0])
+			for button in self.pauseButtons:
+				button.draw(surface)
 		
 	def update(self, events):
 		out = {}
-		for event in events:
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_SPACE:
-					self.worldFocus = not self.worldFocus
-		
-		for button in self.buttons:
-			button.update(events)
+		if not self.paused:
+			for event in events:
+				if event.type == pygame.KEYUP:
+					if event.key == pygame.K_SPACE:
+						self.worldFocus = not self.worldFocus
 			
-		if self.gameOver:
-			self.worldFocus = 1
-			if self.gameOverFade<100:
-				self.gameOverFade+=2
-			for button in self.gameOverButtons:
-				button.update(events)
-		if self.won:
-			self.worldFocus = 1
-			if self.gameOverFade<100:
-				self.gameOverFade+=2
-			for button in self.gameWonButtons:
+			for button in self.buttons:
 				button.update(events)
 				
-		if self.worldFocus == 1:
-			self.ssworld.update(events)
-			self.tdworld.update([])
+			if self.gameOver:
+				self.worldFocus = 1
+				if self.gameOverFade<100:
+					self.gameOverFade+=2
+				for button in self.gameOverButtons:
+					button.update(events)
+			if self.won:
+				self.worldFocus = 1
+				if self.gameOverFade<100:
+					self.gameOverFade+=2
+				for button in self.gameWonButtons:
+					button.update(events)
+					
+			if self.worldFocus == 1:
+				self.ssworld.update(events)
+				self.tdworld.update([])
+			else:
+				self.ssworld.update([])
+				self.tdworld.update(events)
 		else:
-			self.ssworld.update([])
-			self.tdworld.update(events)
+			for button in self.pauseButtons:
+				button.update(events)
 		out["title"] = "Save Our City - "+("Battle View"*self.worldFocus)+("Resource View"*(not self.worldFocus))
 		if self.goBack:
 			out["state"] = "menu"
@@ -79,7 +92,13 @@ class game():
 		return out
 		
 	def clickHandler(self, button):
-		if button == "menu":
-			self.goBack = True
-		if button == "switch":
-			self.worldFocus = not self.worldFocus
+		if self.paused == False:
+			if button == "menu":
+				self.paused = True
+			if button == "switch":
+				self.worldFocus = not self.worldFocus
+		else:
+			if button == "menu":
+				self.goBack = True
+			if button == "back":
+				self.paused = False
