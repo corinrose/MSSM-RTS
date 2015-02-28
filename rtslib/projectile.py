9c19edc6af857ae20a1428b60147ad9e0b592a7a
Andrew Reilley <areill1337@gmail.com>
2015-02-28 01:26:01 -0500

    Added burn effects, effects are now dictionaries

--- rtslib/projectile.py
+++ rtslib/projectile.py
@@ -45,9 +45,16 @@ class projectile():
 				elif self.properties["multitarget"]:			
 					if distance(self.pos[0], self.pos[1], ent.pos[0], ent.pos[1])<self.properties["spreadrange"]:
 						hits.append(ent)
-			for ent in hits:	
-				if self.properties["onhit"]=="damage":
+			for ent in hits:
+				if self.properties["onhit"] == "damage":
 					ent.health -= self.properties["damage"]
-				if self.properties["onhit"]=="slow":
-					ent.applyEffect(["slow", self.properties["percent"], self.properties["time"]])
+				if self.properties["onhit"] == "slow":
+					ent.applyEffect({"type":"slow", 
+									 "percent":self.properties["percent"],
+									 "time":self.properties["time"]})
+				if self.properties["onhit"] == "burn":
+					ent.applyEffect({"type":"burn",
+									 "damage":self.properties["damage"],
+									 "pause":self.properties["pause"],
+									 "hits":self.properties["hits"]})
 			self.remove = True
